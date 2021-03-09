819dd87f3a2b0370e875ab09c1e4502edd931cf1
hkh1213 <hkh1213>
2021-03-09 17:05:46 +0900

    Updates

---
+++ precache-manifest.4788684b3ac96a0316459134751cecbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e1de3276470758541aa56ddfabb53e",
+    "url": "/ReactFolio/index.html"
+  },
+  {
+    "revision": "1c05cdac9494faa86bad",
+    "url": "/ReactFolio/static/css/main.7a95f607.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/ReactFolio/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ReactFolio/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c05cdac9494faa86bad",
+    "url": "/ReactFolio/static/js/main.219f73f7.chunk.js"
+  },
+  {
+    "revision": "cc897b93664c64c1287e",
+    "url": "/ReactFolio/static/js/runtime-main.20217e57.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/ReactFolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/ReactFolio/static/media/your photo.b9d7d448.png"
+  }
+]);
